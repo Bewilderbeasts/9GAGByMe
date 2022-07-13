d03c6a317df64ec6ca95eb3c4503a3f276d62c79
Bewilderbeasts <61983528+Bewilderbeasts@users.noreply.github.com>
2022-07-13 19:50:31 +0200

    CSS changes, created simple main page for planned merging front- with backend.

--- fn-front/src/App.js
+++ fn-front/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
 import RegistrationForm from './registry/Register';
 import LoginForm from './registry/Login';
+import WelcomeBack from './pages/Main';
 
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
                     <Routes>
                         <Route path='/register' element={<RegistrationForm />}/>
                         <Route path='/login' element={<LoginForm />} />
+                        <Route path='/' element={<WelcomeBack />} />
                     </Routes>
                 </div>
             </Router>
---
+++ fn-front/src/pages/Main.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './main.css';
+
+function WelcomeBack() {
+    return (       
+        <div class="site" className="welcome">
+            <div class="content" className="welcome-left">
+                <div className="foreground" >
+                <div className="returnmessage">
+                Welcome back!
+                </div>
+                </div>
+            </div>         
+        </div>
+    )
+}
+export default WelcomeBack;
